7a1eedac885ccb3522b7cd1dfa5e1c19ca985f9e
Jean Rodriguez <jrodriguezfe@senati.pe>
2026-05-27 16:59:40 -0500

    act

--- script.js
+++ script.js
@@ -2120,7 +2120,7 @@ window.prepareForExport = function() {
 
 window.exportarAExcel = function() {
     window.prepareForExport();
-    const element = document.getElementById('areaEficacia').cloneNode(true);
+    const element = document.getElementById('contenedorTablasEficacia').cloneNode(true);
     element.querySelectorAll('button').forEach(btn => btn.remove());
     
     let preHtml = `<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><meta charset="utf-8"><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>Eficacia</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body>`;
